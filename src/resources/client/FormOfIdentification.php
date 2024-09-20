7e1fb37d8e4d79418d625fe689f54ec1b20054c0
Jóhan Dam-Davidsen <johan.davidsen@fjakkarin.com>
2024-09-20 14:16:15 +0100

    Added documentation

--- src/resources/client/FormOfIdentification.php
+++ src/resources/client/FormOfIdentification.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This file contains a class to represent a form of identification in the Amadeus API.
+ *
+ * @namespace Amadeus\Resources\Client
+ * @package Amadeus\Resources\Client
+ */
 
 namespace Amadeus\Resources\Client;
 
@@ -9,6 +15,10 @@ const BOOKING_CONFIRMATION = 'BOOKING_CONFIRMATION';
 const TICKET = 'TICKET';
 const OTHER_ID = 'OTHER_ID';
 
+/**
+ * Class FormOfIdentification
+ * @package Amadeus\Resources\Client
+ */
 class FormOfIdentification
 {
 	private string $identificationType;
@@ -17,6 +27,9 @@ class FormOfIdentification
 	private array $travelerIds;
 	private array $flightOfferIds;
 
+	/**
+	 * FormOfIdentification constructor.
+	 */
 	public function __construct() {
 		$this->identificationType = DRIVERS_LICENSE;
 		$this->carrierCode = '';
@@ -25,51 +38,106 @@ class FormOfIdentification
 		$this->flightOfferIds = [];
 	}
 
+	/**
+	 * Set the identification type to driver's license.
+	 *
+	 * @return FormOfIdentification
+	 */
 	public function setIdentificationTypeToDriversLicense(): FormOfIdentification {
 		$this->identificationType = DRIVERS_LICENSE;
 		return $this;
 	}
 
+	/**
+	 * Set the identification type to passport.
+	 *
+	 * @return FormOfIdentification
+	 */
 	public function setIdentificationTypeToPassport(): FormOfIdentification {
 		$this->identificationType = PASSPORT;
 		return $this;
 	}
 
+	/**
+	 * Set the identification type to national identity card.
+	 *
+	 * @return FormOfIdentification
+	 */
 	public function setIdentificationTypeToNationalIdentityCard(): FormOfIdentification {
 		$this->identificationType = NATIONAL_IDENTITY_CARD;
 		return $this;
 	}
 
+	/**
+	 * Set the identification type to booking confirmation.
+	 *
+	 * @return FormOfIdentification
+	 */
 	public function setIdentificationTypeToBookingConfirmation(): FormOfIdentification {
 		$this->identificationType = BOOKING_CONFIRMATION;
 		return $this;
 	}
 
+	/**
+	 * Set the identification type to ticket.
+	 *
+	 * @return FormOfIdentification
+	 */
 	public function setIdentificationTypeToTicket(): FormOfIdentification {
 		$this->identificationType = TICKET;
 		return $this;
 	}
 
+	/**
+	 * Set the identification type to other ID.
+	 *
+	 * @return FormOfIdentification
+	 */
 	public function setIdentificationTypeToOtherId(): FormOfIdentification {
 		$this->identificationType = OTHER_ID;
 		return $this;
 	}
 
+	/**
+	 * Set the carrier code.
+	 *
+	 * @param string $carrierCode
+	 * @return FormOfIdentification
+	 */
 	public function setCarrierCode(string $carrierCode): FormOfIdentification {
 		$this->carrierCode = $carrierCode;
 		return $this;
 	}
 
+	/**
+	 * Set the number.
+	 *
+	 * @param string $number
+	 * @return FormOfIdentification
+	 */
 	public function setNumber(string $number): FormOfIdentification {
 		$this->number = $number;
 		return $this;
 	}
 
+	/**
+	 * Add a traveler ID.
+	 *
+	 * @param string $travelerId
+	 * @return FormOfIdentification
+	 */
 	public function addTravelerId(string $travelerId): FormOfIdentification {
 		$this->travelerIds[] = $travelerId;
 		return $this;
 	}
 
+	/**
+	 * Remove a traveler ID.
+	 *
+	 * @param string $travelerId
+	 *
+	 * @return FormOfIdentification
+	 */
 	public function removeTravelerId(string $travelerId): FormOfIdentification {
 		$key = array_search($travelerId, $this->travelerIds);
 		if ($key !== false) {
@@ -78,11 +146,23 @@ class FormOfIdentification
 		return $this;
 	}
 
+	/**
+	 * Add a flight offer ID.
+	 *
+	 * @param string $flightOfferId
+	 * @return FormOfIdentification
+	 */
 	public function addFlightOfferId(string $flightOfferId): FormOfIdentification {
 		$this->flightOfferIds[] = $flightOfferId;
 		return $this;
 	}
 
+	/**
+	 * Remove a flight offer ID.
+	 *
+	 * @param string $flightOfferId
+	 * @return FormOfIdentification
+	 */
 	public function removeFlightOfferId(string $flightOfferId): FormOfIdentification {
 		$key = array_search($flightOfferId, $this->flightOfferIds);
 		if ($key !== false) {
@@ -91,6 +171,11 @@ class FormOfIdentification
 		return $this;
 	}
 
+	/**
+	 * Convert the object to an array.
+	 *
+	 * @return array
+	 */
 	public function __toArray(): array {
 		return [
 			'identificationType' => $this->identificationType,
