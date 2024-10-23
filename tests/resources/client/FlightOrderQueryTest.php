d5181bf6b893898a4a6c04519691a698f76e9fca
Jóhan Dam-Davidsen <johan.davidsen@fjakkarin.com>
2024-10-23 10:54:07 -0100

    Fixed style

--- tests/resources/client/FlightOrderQueryTest.php
+++ tests/resources/client/FlightOrderQueryTest.php
@@ -27,126 +27,126 @@ use PHPUnit\Framework\TestCase;
  * @package Amadeus\Tests\resources\client
  */
 #[
-	CoversClass(FlightOrderQuery::class)
+    CoversClass(FlightOrderQuery::class)
 ]
 class FlightOrderQueryTest extends TestCase
 {
     public function test_basic_flight_order_query()
     {
-		$officeId = 'GOHS12101';
+        $officeId = 'GOHS12101';
 
         $flightOrderQuery = new FlightOrderQuery();
 
         $this->assertEquals('flight-order', $flightOrderQuery->getType());
 
-		$flightOrderQuery->setQueuingOfficeId($officeId);
-		$this->assertEquals($officeId , $flightOrderQuery->getQueuingOfficeId());
+        $flightOrderQuery->setQueuingOfficeId($officeId);
+        $this->assertEquals($officeId, $flightOrderQuery->getQueuingOfficeId());
 
-		$flightOrderQuery->setOwnerOfficeId($officeId);
-		$this->assertEquals($officeId , $flightOrderQuery->getOwnerOfficeId());
+        $flightOrderQuery->setOwnerOfficeId($officeId);
+        $this->assertEquals($officeId, $flightOrderQuery->getOwnerOfficeId());
 
-	    /**
-	     * Test Flight Offer setter and getter
-	     */
-		$flightOffersBody = PHPUnitUtil::readFile(
-		    PHPUnitUtil::RESOURCE_PATH_ROOT . "flight_offers_get_response_ok.json"
-	    );
-		// Map to FlightOffers
-		$flightOffers = Resource::toResourceArray(json_decode($flightOffersBody, false)->data, FlightOffer::class);
-		// Add first flight offer
-		$flightOrderQuery->addFlightOffer($flightOffers[0]);
+        /**
+         * Test Flight Offer setter and getter
+         */
+        $flightOffersBody = PHPUnitUtil::readFile(
+            PHPUnitUtil::RESOURCE_PATH_ROOT . "flight_offers_get_response_ok.json"
+        );
+        // Map to FlightOffers
+        $flightOffers = Resource::toResourceArray(json_decode($flightOffersBody, false)->data, FlightOffer::class);
+        // Add first flight offer
+        $flightOrderQuery->addFlightOffer($flightOffers[0]);
 
-	    /**
-	     * Test Traveler setter and getter
-	     * Get the Traveler object from the FlightOrder
-	     */
-		$flightOrderBody = PHPUnitUtil::readFile(
-			PHPUnitUtil::RESOURCE_PATH_ROOT . "flight_orders_post_request_ok.json"
-		);
-		// Map to FlightOrder
-		$flightOrder = Resource::toResourceObject(json_decode($flightOrderBody, false)->data, FlightOrder::class);
+        /**
+         * Test Traveler setter and getter
+         * Get the Traveler object from the FlightOrder
+         */
+        $flightOrderBody = PHPUnitUtil::readFile(
+            PHPUnitUtil::RESOURCE_PATH_ROOT . "flight_orders_post_request_ok.json"
+        );
+        // Map to FlightOrder
+        $flightOrder = Resource::toResourceObject(json_decode($flightOrderBody, false)->data, FlightOrder::class);
 
-		// Add first traveler
-		$flightOrderQuery->addTraveler($flightOrder->getTravelers()[0]);
+        // Add first traveler
+        $flightOrderQuery->addTraveler($flightOrder->getTravelers()[0]);
 
-		//
-	    $generalRemark = new GeneralRemark();
-		$generalRemark->setGeneralMiscellaneous();
-		$generalRemark->setText('This is a test remark');
-		$generalRemark->setCategory('General');
-		$generalRemark->addTravelerId('1');
-		$generalRemark->addTravelerId('2');
-		$generalRemark->addFlightOfferId('1');
+        //
+        $generalRemark = new GeneralRemark();
+        $generalRemark->setGeneralMiscellaneous();
+        $generalRemark->setText('This is a test remark');
+        $generalRemark->setCategory('General');
+        $generalRemark->addTravelerId('1');
+        $generalRemark->addTravelerId('2');
+        $generalRemark->addFlightOfferId('1');
 
-		$airlineRemark = new AirlineRemark();
-		$airlineRemark->setAirlineCode('AA');
-		$airlineRemark->setText('This is an airline remark');
-	    $generalRemark->addTravelerId('1');
-		$airlineRemark->addFlightOfferId('1');
+        $airlineRemark = new AirlineRemark();
+        $airlineRemark->setAirlineCode('AA');
+        $airlineRemark->setText('This is an airline remark');
+        $generalRemark->addTravelerId('1');
+        $airlineRemark->addFlightOfferId('1');
 
-	    $remarks = new Remarks();
-		$remarks->addGeneralRemark($generalRemark);
-		$remarks->addAirlineRemark($airlineRemark);
-		$flightOrderQuery->setRemarks($remarks);
+        $remarks = new Remarks();
+        $remarks->addGeneralRemark($generalRemark);
+        $remarks->addAirlineRemark($airlineRemark);
+        $flightOrderQuery->setRemarks($remarks);
 
-		$ticketingAgreement = new TicketingAgreement();
-		$ticketingAgreement->setOptionToConfirm();
-		$flightOrderQuery->setTicketingAgreement($ticketingAgreement);
+        $ticketingAgreement = new TicketingAgreement();
+        $ticketingAgreement->setOptionToConfirm();
+        $flightOrderQuery->setTicketingAgreement($ticketingAgreement);
 
-		$automatedProcess = new AutomatedProcess();
-		$automatedProcess->setCodeToImmediate();
-		$queue = new Queue();
-		$queue->setNumber('1');
-		$queue->setCategory('A');
-		$automatedProcess->setQueue($queue);
-		$automatedProcess->setText('This is an automated process');
-		$flightOrderQuery->addAutomatedProcess($automatedProcess);
+        $automatedProcess = new AutomatedProcess();
+        $automatedProcess->setCodeToImmediate();
+        $queue = new Queue();
+        $queue->setNumber('1');
+        $queue->setCategory('A');
+        $automatedProcess->setQueue($queue);
+        $automatedProcess->setText('This is an automated process');
+        $flightOrderQuery->addAutomatedProcess($automatedProcess);
 
-		$traveler = new TravelerContact();
+        $traveler = new TravelerContact();
 
-		$name = new TravelerName();
-		$name->__set('firstName', 'John');
-		$name->__set('lastName', 'Doe');
-		$traveler->__set('addresseeName', $name);
+        $name = new TravelerName();
+        $name->__set('firstName', 'John');
+        $name->__set('lastName', 'Doe');
+        $traveler->__set('addresseeName', $name);
 
-		$traveler->__set('companyName', 'Acme Inc');
-		$traveler->__set('purpose', 'INVOICE');
+        $traveler->__set('companyName', 'Acme Inc');
+        $traveler->__set('purpose', 'INVOICE');
 
-		$phone = new TravelerPhone();
-		$phone->__set('number', '1234567890');
-		$phone->__set('purpose', 'MOBILE');
-		$traveler->__set('phones', [$phone]);
+        $phone = new TravelerPhone();
+        $phone->__set('number', '1234567890');
+        $phone->__set('purpose', 'MOBILE');
+        $traveler->__set('phones', [$phone]);
 
-		$traveler->__set('emailAddress', 'blob@blob.gl');
+        $traveler->__set('emailAddress', 'blob@blob.gl');
 
-		$address = new TravelerAddress();
-		$address->__set('lines', ['123 Main St']);
-		$address->__set('cityName', 'Anytown');
-		$address->__set('stateName', 'NY');
-		$address->__set('postalCode', '12345');
-		$address->__set('countryCode', 'US');
-		$traveler->__set('address', $address);
+        $address = new TravelerAddress();
+        $address->__set('lines', ['123 Main St']);
+        $address->__set('cityName', 'Anytown');
+        $address->__set('stateName', 'NY');
+        $address->__set('postalCode', '12345');
+        $address->__set('countryCode', 'US');
+        $traveler->__set('address', $address);
 
-		$traveler->__set('language', 'EN');
-		$flightOrderQuery->addContact($traveler);
+        $traveler->__set('language', 'EN');
+        $flightOrderQuery->addContact($traveler);
 
-		$formOfIdentification = new FormOfIdentification();
-		$formOfIdentification->setIdentificationTypeToTicket();
-		$formOfIdentification->setCarrierCode('AA');
-		$formOfIdentification->setNumber('1234567890');
-		$formOfIdentification->addTravelerId('1');
-		$formOfIdentification->addFlightOfferId('1');
-		$flightOrderQuery->addFormOfIdentification($formOfIdentification);
+        $formOfIdentification = new FormOfIdentification();
+        $formOfIdentification->setIdentificationTypeToTicket();
+        $formOfIdentification->setCarrierCode('AA');
+        $formOfIdentification->setNumber('1234567890');
+        $formOfIdentification->addTravelerId('1');
+        $formOfIdentification->addFlightOfferId('1');
+        $flightOrderQuery->addFormOfIdentification($formOfIdentification);
 
-		$creditCard = new RegularCreditCard();
-		$creditCard->setAsVisa();
-		$creditCard->setNumber('1234567890');
-		$creditCard->setHolder('John Doe');
-		$creditCard->setExpiryDate('2023-12');
-		$creditCard->setSecurityCode('123');
-		$creditCard->addFlightOfferIds('1');
-		$flightOrderQuery->setFormOfPayment($creditCard);
+        $creditCard = new RegularCreditCard();
+        $creditCard->setAsVisa();
+        $creditCard->setNumber('1234567890');
+        $creditCard->setHolder('John Doe');
+        $creditCard->setExpiryDate('2023-12');
+        $creditCard->setSecurityCode('123');
+        $creditCard->addFlightOfferIds('1');
+        $flightOrderQuery->setFormOfPayment($creditCard);
 
-		$this->assertEquals($flightOrderQuery->getType(), 'flight-order');
+        $this->assertEquals($flightOrderQuery->getType(), 'flight-order');
     }
 }
